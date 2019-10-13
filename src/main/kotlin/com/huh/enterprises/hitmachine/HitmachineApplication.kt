106cd8e642aaeb620dce9d3904dcb7e5c984337e
Jun Huh <45692017+junhuhdev@users.noreply.github.com>
2019-10-13 13:22:50 +0200

    Initial commit

---
+++ src/main/kotlin/com/huh/enterprises/hitmachine/HitmachineApplication.kt
@@ -0,0 +1,11 @@
+package com.huh.enterprises.hitmachine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HitmachineApplication
+
+fun main(args: Array<String>) {
+	runApplication<HitmachineApplication>(*args)
+}
---
+++ src/main/kotlin/com/huh/enterprises/hitmachine/web/MovieController.kt
@@ -0,0 +1,4 @@
+package com.huh.enterprises.hitmachine.web
+
+class MovieController {
+}
